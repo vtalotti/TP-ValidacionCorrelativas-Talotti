be33184dcdcafed784b35e4fd3a58527b35ea94e
Valentino <vtalotti@frba.utn.edu.ar>
2025-04-01 22:22:57 -0300

    Update Alumno.java
    AlumnosJava

--- src/main/java/org/example/Alumno.java
+++ src/main/java/org/example/Alumno.java
@@ -1,4 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumno {
+    private String nombre;
+    private List<Materia> materiasAprobadas;
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+        this.materiasAprobadas = new ArrayList<>();
+    }
+
+    public void aprobarMateria(Materia materia) {
+        this.materiasAprobadas.add(materia);
+    }
+
+    public boolean tieneAprobada(Materia materia) {
+        return materiasAprobadas.contains(materia);
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Materia> getMateriasAprobadas() {
+        return materiasAprobadas;
+    }
+
 }
