767f2f1c77ac593f3606084e67a16d38edc43061
Valentino <vtalotti@frba.utn.edu.ar>
2025-04-01 22:23:46 -0300

    Update Inscripcion.java
    inscripcionesJava

--- src/main/java/org/example/Inscripcion.java
+++ src/main/java/org/example/Inscripcion.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.util.List;
+
 public class Inscripcion {
+    private Alumno alumno;
+    private List<Materia> materias;
+
+    public Inscripcion(Alumno alumno, List<Materia> materias) {
+        this.alumno = alumno;
+        this.materias = materias;
+    }
+
+    public boolean aprobada() {
+        for (Materia materia : materias) {
+            if (!materia.puedeCursar(alumno)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
