cdfa7d7ddc670a6ecd0c924d43ef782e7451c414
Valentino <vtalotti@frba.utn.edu.ar>
2025-04-01 22:24:12 -0300

    Update Main.java
    mainJava

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,35 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Creación de materias
+        Materia algoritmos = new Materia("Algoritmos y Estructuras de Datos");
+        Materia paradigmas = new Materia("Paradigmas de Programación");
+        Materia diseño = new Materia("Diseño de Sistemas");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Establecer correlativas
+        paradigmas.agregarCorrelativa(algoritmos);
+        diseño.agregarCorrelativa(paradigmas);
+
+        // Creación de alumno
+        Alumno juan = new Alumno("Juan Perez");
+        juan.aprobarMateria(algoritmos);
+        juan.aprobarMateria(paradigmas);
+
+        // Creación de inscripción
+        List<Materia> materiasACursar = new ArrayList<>();
+        materiasACursar.add(diseño);
+
+        Inscripcion inscripcion = new Inscripcion(juan, materiasACursar);
+
+        // Validar inscripción
+        if (inscripcion.aprobada()) {
+            System.out.println("La inscripción fue aprobada");
+        } else {
+            System.out.println("La inscripción fue rechazada");
         }
     }
 }
