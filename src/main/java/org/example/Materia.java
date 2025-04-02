c7f9048cf755b268517a0e391f02540c23381b4d
Valentino <vtalotti@frba.utn.edu.ar>
2025-04-01 22:24:31 -0300

    Update Materia.java
    materiaJAva

--- src/main/java/org/example/Materia.java
+++ src/main/java/org/example/Materia.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Materia {
-}
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<>();
+    }
+
+    public void agregarCorrelativa(Materia materia) {               //para testear
+        this.correlativas.add(materia);
+    }
+
+    public boolean puedeCursar(Alumno alumno) {
+        for (Materia correlativa : correlativas) {
+            if (!alumno.tieneAprobada(correlativa)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+}}
